0290a6708b32cfc5f42e75e42d38b29c589319da
pmayur <thisispatil@gmail.com>
2019-05-20 03:20:30 +0530

    looping a triangle

--- loopingATriangle.js
+++ loopingATriangle.js
@@ -9,11 +9,11 @@ triangle:
 ######
 #######
 */
-var hash = "";
+let hash = "";
 for( var i = 1; i <= 7 ; i++ ){
     for( var j = 1; j <= i; j++){
         hash += "#";
     }
     console.log(hash)
-    var hash = "";
+    let hash = "";
 }
