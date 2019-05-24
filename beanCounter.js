e6c89e3f255c6b023100adb4be9ebb4bbaaa6ea9
pmayur <thisispatil@gmail.com>
2019-05-25 04:35:04 +0530

    bean counter

---
+++ beanCounter.js
@@ -0,0 +1,36 @@
+/* Bean counting
+You can get the Nth character, or letter, from a string by writing "string"[N] .
+The returned value will be a string containing only one character (for example,
+"b" ). The first character has position 0, which causes the last one to be found at
+position string.length - 1 . In other words, a two-character string has length
+2, and its characters have positions 0 and 1.
+Write a function countBs that takes a string as its only argument and returns
+a number that indicates how many uppercase “B” characters there are in the
+string.
+Next, write a function called countChar that behaves like countBs , except
+it takes a second argument that indicates the character that is to be counted
+(rather than counting only uppercase “B” characters). Rewrite countBs to
+make use of this new function. */
+
+function countBs(val){
+    var count = 0;
+    for (var i = 0; i < val.length; i++){
+        if(val[i] == "B"){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countBs("BiBsa"))
+
+function countChar(val, find){
+    var count = 0;
+    for (var i = 0; i < val.length; i++){
+        if(val[i] == find){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countChar("alphabeta", "a"))
